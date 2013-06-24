52d04d2db822c621a3d0f7894a3f026715493eb1
Eric Lombrozo <elombrozo@gmail.com>
2013-06-23 19:14:11 -0700

    Pulled CheckBlock out of CBlock.
    Conflicts:
    	src/main.cpp

--- src/main.h
+++ src/main.h
@@ -799,9 +799,6 @@ public:
     }
 
 
-    // Context-independent validity checks
-    bool CheckBlock(CValidationState &state, bool fCheckPOW=true, bool fCheckMerkleRoot=true, bool fCheckSig=true) const;
-
     // Store block on disk
     // if dbp is provided, the file is known to already reside on disk
     bool AcceptBlock(CValidationState &state, CDiskBlockPos *dbp = NULL);
@@ -832,6 +829,9 @@ bool ConnectBlock(CBlock& block, CValidationState& state, CBlockIndex* pindex, C
 
 // Add this block to the block index, and if necessary, switch the active block chain to this
 bool AddToBlockIndex(CBlock& block, CValidationState &state, const CDiskBlockPos &pos, const uint256 &hashProof);
+
+// Context-independent validity checks
+bool CheckBlock(const CBlock& block, CValidationState &state, bool fCheckPOW=true, bool fCheckMerkleRoot=true, bool fCheckSig=true);
 
 class CBlockFileInfo
 {
