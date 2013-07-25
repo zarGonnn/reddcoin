0592daa533619445d2f72693ef69f038300f9fb7
Pieter Wuille <pieter.wuille@gmail.com>
2013-07-25 02:25:25 +0200

    Run node deletions outside of cs_vNodes

--- src/net.cpp
+++ src/net.cpp
@@ -803,7 +803,8 @@ void ThreadSocketHandler()
                     vNodesDisconnected.push_back(pnode);
                 }
             }
-
+        }
+        {
             // Delete disconnected nodes
             list<CNode*> vNodesDisconnectedCopy = vNodesDisconnected;
             BOOST_FOREACH(CNode* pnode, vNodesDisconnectedCopy)
