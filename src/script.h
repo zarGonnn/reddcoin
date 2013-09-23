5e95c4809d99039e4f6a5fe070e3a0500cd9d322
Pieter Wuille <sipa@ulyssis.org>
2013-09-24 00:45:18 +0200

    Move IsPushOnly() to script.cpp
    Conflicts:
    	src/script.cpp
    	src/script.h

--- src/script.h
+++ src/script.h
@@ -542,23 +542,7 @@ public:
     bool IsPayToScriptHash() const;
 
     // Called by IsStandardTx and P2SH VerifyScript (which makes it consensus-critical).
-    bool IsPushOnly() const
-    {
-        const_iterator pc = begin();
-        while (pc < end())
-        {
-            opcodetype opcode;
-            if (!GetOp(pc, opcode))
-                return false;
-            // Note that IsPushOnly() *does* consider OP_RESERVED to be a
-            // push-type opcode, however execution of OP_RESERVED fails, so
-            // it's not relevant to P2SH as the scriptSig would fail prior to
-            // the P2SH special validation code being executed.
-            if (opcode > OP_16)
-                return false;
-        }
-        return true;
-    }
+    bool IsPushOnly() const;
 
     // Called by IsStandardTx.
     bool HasCanonicalPushes() const;
