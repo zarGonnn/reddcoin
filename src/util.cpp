3c38f2ac1fe150f6fc2bf3c2278663e1968885b5
Philip Kaufmann <phil.kaufmann@t-online.de>
2013-10-08 10:52:42 +0200

    make fCommandLine a local variable in AppInit()

--- src/util.cpp
+++ src/util.cpp
@@ -78,7 +78,6 @@ bool fPrintToConsole = false;
 bool fPrintToDebugger = false;
 bool fDaemon = false;
 bool fServer = false;
-bool fCommandLine = false;
 string strMiscWarning;
 bool fBloomFilters = true;
 bool fNoListen = false;
