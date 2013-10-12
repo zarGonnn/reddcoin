7990386d329cdde2844f82964bd463e84944169a
Pieter Wuille <pieter.wuille@gmail.com>
2013-10-12 15:22:33 +0200

    Move CBlockLocator to core.h
    As CBlockLocator is a P2P data structure, and independent from the
    validation logic, it can be moved to core.

--- src/main.h
+++ src/main.h
@@ -25,7 +25,6 @@ class CBlockHeader;
 class CKeyItem;
 class CReserveKey;
 class COutPoint;
-class CBlockLocator;
 
 class CAddress;
 class CInv;
@@ -1203,43 +1202,6 @@ public:
 /** The currently-connected chain of blocks. */
 extern CChain chainActive;
 
-
-
-/** Describes a place in the block chain to another node such that if the
- * other node doesn't have the same branch, it can find a recent common trunk.
- * The further back it is, the further before the fork it may be.
- */
-class CBlockLocator
-{
-protected:
-    std::vector<uint256> vHave;
-public:
-    CBlockLocator() {}
-
-    CBlockLocator(const std::vector<uint256>& vHaveIn)
-    {
-        vHave = vHaveIn;
-    }
-
-    IMPLEMENT_SERIALIZE
-    (
-        if (!(nType & SER_GETHASH))
-            READWRITE(nVersion);
-        READWRITE(vHave);
-    )
-
-    void SetNull()
-    {
-        vHave.clear();
-    }
-
-    bool IsNull()
-    {
-        return vHave.empty();
-    }
-
-    friend class CChain;
-};
 
 
 
