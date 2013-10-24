4a09f166255fc31adf2735386ac3d66b29f3545f
Philip Kaufmann <phil.kaufmann@t-online.de>
2013-10-24 09:04:46 +0200

    [Qt]: fix num Blocks to maturity in Tx description
    - as we have main.h included in transactiondesc.cpp, we now also use
      COINBASE_MATURITY +  1 for the descriptive string
    - fixes #3131
    
    Conflicts:
    	src/qt/transactiondesc.cpp

--- src/qt/transactiondesc.cpp
+++ src/qt/transactiondesc.cpp
@@ -239,8 +239,11 @@ QString TransactionDesc::toHTML(CWallet *wallet, CWalletTx &wtx, int vout, int u
             }
         }
 
-        if (wtx.IsCoinBase() || wtx.IsCoinStake())
-            strHTML += "<br>" + tr("Generated coins must mature 50 blocks before they can be spent. When you generated this block, it was broadcast to the network to be added to the block chain. If it fails to get into the chain, its state will change to \"not accepted\" and it won't be spendable. This may occasionally happen if another node generates a block within a few seconds of yours.") + "<br>";
+        if (wtx.IsCoinBase())
+        {
+            quint32 numBlocksToMaturity = COINBASE_MATURITY +  1;
+            strHTML += "<br>" + tr("Generated coins must mature %1 blocks before they can be spent. When you generated this block, it was broadcast to the network to be added to the block chain. If it fails to get into the chain, its state will change to \"not accepted\" and it won't be spendable. This may occasionally happen if another node generates a block within a few seconds of yours.").arg(QString::number(numBlocksToMaturity)) + "<br>";
+        }
 
         //
         // Debug view
