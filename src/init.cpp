cd1639289da97441b22917156d5191b0b63b2cc1
Wladimir J. van der Laan <laanwj@gmail.com>
2013-12-16 18:13:31 +0100

    qt: Prevent non-functional GUI from popping up during Init
    When a InitError or InitWarning happens, the
    GUI pops up but is unusable (until Init finishes).
    
    This is caused by showNormalIfMinimized. Add a message
    flag to skip this call for Init errors or warnings.

--- src/init.cpp
+++ src/init.cpp
@@ -169,13 +169,13 @@ void HandleSIGHUP(int)
 
 bool static InitError(const std::string &str)
 {
-    uiInterface.ThreadSafeMessageBox(str, "", CClientUIInterface::MSG_ERROR);
+    uiInterface.ThreadSafeMessageBox(str, "", CClientUIInterface::MSG_ERROR | CClientUIInterface::NOSHOWGUI);
     return false;
 }
 
 bool static InitWarning(const std::string &str)
 {
-    uiInterface.ThreadSafeMessageBox(str, "", CClientUIInterface::MSG_WARNING);
+    uiInterface.ThreadSafeMessageBox(str, "", CClientUIInterface::MSG_WARNING | CClientUIInterface::NOSHOWGUI);
     return true;
 }
 
