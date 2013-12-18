db8b6b2b84e8bcdec65177c1975f9f01f7821bdb
Wladimir J. van der Laan <laanwj@gmail.com>
2013-12-18 09:45:36 +0100

    qt: Treat regtest as testnet
    No need to do anything special in the GUI for regtest mode,
    but do treat it at testnet not mainnet to prevent confusion.
    
    Conflicts:
    	src/qt/bitcoin.cpp
    	src/qt/splashscreen.cpp

--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -4,14 +4,13 @@
 
 #include "splashscreen.h"
 
-#include "chainparams.h"
 #include "clientversion.h"
 #include "util.h"
 
 #include <QApplication>
 #include <QPainter>
 
-SplashScreen::SplashScreen(const QPixmap &pixmap, Qt::WindowFlags f) :
+SplashScreen::SplashScreen(const QPixmap &pixmap, Qt::WindowFlags f, bool isTestNet) :
     QSplashScreen(pixmap, f)
 {
     // set reference point, paddings
@@ -35,7 +34,7 @@ SplashScreen::SplashScreen(const QPixmap &pixmap, Qt::WindowFlags f) :
 
     // load the bitmap for writing some text over it
     QPixmap newPixmap;
-    if(TestNet()) {
+    if(isTestNet) {
         newPixmap     = QPixmap(":/images/splash_testnet");
     }
     else {
