590a1e5e76fc5060c25cb8240432be8bca2370ad
Wladimir J. van der Laan <laanwj@gmail.com>
2014-02-13 17:28:32 +0100

    Add raw transaction hex to `gettransaction` wallet RPC
    This allows getting raw transaction data from the wallet even if the
    transaction is no longer in the blockchain / mempool (for example if it
    got orphaned due to malleability abuse).

--- src/rpcwallet.cpp
+++ src/rpcwallet.cpp
@@ -1481,7 +1481,8 @@ Value gettransaction(const Array& params, bool fHelp)
             "      \"amount\" : x.xxx                  (numeric) The amount in RDD\n"
             "    }\n"
             "    ,...\n"
-            "  ]\n"
+            "  ],\n"
+            "  \"hex\" : \"data\"         (string) Raw data for transaction\n"
             "}\n"
 
             "\nbExamples\n"
@@ -1511,6 +1512,11 @@ Value gettransaction(const Array& params, bool fHelp)
     Array details;
     ListTransactions(wtx, "*", 0, false, details);
     entry.push_back(Pair("details", details));
+
+    CDataStream ssTx(SER_NETWORK, PROTOCOL_VERSION);
+    ssTx << wtx;
+    string strHex = HexStr(ssTx.begin(), ssTx.end());
+    entry.push_back(Pair("hex", strHex));
 
     return entry;
 }
