89279c82322297f14d159206b634f05e73aec028
Wladimir J. van der Laan <laanwj@gmail.com>
2014-02-21 16:31:49 +0100

    Remove "conflicted" as transaction category.
    We were losing information about sent/received by overriding the
    category in case of a conflicted transaction.
    
    Hence, remove the "conflicted" category.
    
    Conflicted status of a transaction can still be determined by looking
    for confirmations<0.

--- src/rpcwallet.cpp
+++ src/rpcwallet.cpp
@@ -1134,10 +1134,7 @@ void ListTransactions(const CWalletTx& wtx, const string& strAccount, int nMinDe
             Object entry;
             entry.push_back(Pair("account", strSentAccount));
             MaybePushAddress(entry, s.first);
-            if (wtx.GetDepthInMainChain() < 0)
-                entry.push_back(Pair("category", "conflicted"));
-            else
-                entry.push_back(Pair("category", "send"));
+            entry.push_back(Pair("category", "send"));
             entry.push_back(Pair("amount", ValueFromAmount(-s.second)));
             entry.push_back(Pair("fee", ValueFromAmount(-nFee)));
             if (fLong)
@@ -1173,10 +1170,7 @@ void ListTransactions(const CWalletTx& wtx, const string& strAccount, int nMinDe
                 }
                 else
                 {
-                    if (wtx.GetDepthInMainChain() < 0)
-                        entry.push_back(Pair("category", "conflicted"));
-                    else
-                        entry.push_back(Pair("category", "receive"));
+                    entry.push_back(Pair("category", "receive"));
                 }
                 if (!wtx.IsCoinStake())
                     entry.push_back(Pair("amount", ValueFromAmount(r.second)));
