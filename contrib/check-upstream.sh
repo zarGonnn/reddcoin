efc615ec0389d7a427499946d715a5a335628bd2
Mathy Vanvoorden <mathy@vanvoorden.be>
2014-04-08 00:23:37 +0200

    Updated check level

--- contrib/check-upstream.sh
+++ contrib/check-upstream.sh
@@ -15,7 +15,7 @@ function check(){
 
 check bitcoin \
 	master \
-	3b7925eb7d0653ab304786e8e2344af91a6826fb
+	5d891489ab7828ad8db15e85bb63e2f13f021a6a
 
 check litecoin \
 	master-0.8 \
@@ -32,19 +32,35 @@ echo "If you don't include a commit for some reason, explain why at the bottom o
 
 # Not merged from Litecoin
 
-570f4b03eba23abb3c09045a673105d956103847 Litecoin: Add checkpoints to testnet
-f389e65c8dcb1544a023caff33da41c71fe61a80 Add testnet DNS seed from xurious.com
 dc4cd268f521ab7ae503692eb2c56f581087ef9f Update Qt 4.8.3 download link
+  Reddcoin has own release documentation
 a8f8323fa9b369f8473511f1c2864ddcac997572 Change release-process.md to sign release tags
+  Reddcoin has own release documentation
 5a79068b5aeca439fc7ef08846b7b36175c0c600 Update build-osx.md
+  Reddcoin has own release documentation
 70313dd1e7518d410dac30db4906aac4846520a7 Litecoin: settxfee label in LTC
+  Committed with RDD labeling
 06e78495d588c5f7d8cc56b6eb379fc9c46880c5 Litecoin: Checkpoint at block 541794
+  Litecoin specific
 41fa6245259bb1edc226c9b49b548d23a6627fc1 Litecoin: Upgrade openssl, qt, miniupnpc, zlib, libpng, qrencode
+  Reddcoin has own gitian
 6ce2f0b2fb4d8a58e86919b25d86a917a6f8808f Litecoin v0.8.6.9
+  Litecoin specific
 aa77ae3a8c31b7bada91e53d451172f01405ce51 Litecoin v0.8.7.1
+  Litecoin specific
 ccd9a1f5f72235244886d108f014d40d2d6c7bd5 Update testnet DNS seeds
+  Litecoin specific
+f389e65c8dcb1544a023caff33da41c71fe61a80 Add testnet DNS seed from xurious.com
+  Litecoin specific
+570f4b03eba23abb3c09045a673105d956103847 Litecoin: Add checkpoints to testnet
+  Litecoin specific, we don't have checkpoints in our testnet (yet)
 
 # Not merged from Bitcoin
+
+8c8132be73b680d04b9f6dff9dee0ae277c1bebb Prepare for 0.9 merge window
+  Release specific stuff
+c25e98186d0f716451ef000e55646d25e014f573 Squashed 'src/leveldb/' content from commit aca1ffc
+  Not possible to merge
 
 
 NOTMERGED
