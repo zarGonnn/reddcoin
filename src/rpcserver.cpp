66f660aeaca167d35e000752652753f324d4912b
Wladimir J. van der Laan <laanwj@gmail.com>
2014-05-07 09:09:13 +0200

    rpc: pass errors from async_accept
    According to the [boost::asio documentation](http://www.boost.org/doc/libs/1_55_0/doc/html/boost_asio/reference/basic_socket_acceptor/async_accept/overload2.html),
    the function signature of the handler must be:
    
        void handler(
          const boost::system::error_code& error // Result of operation.
        );
    
    We were binding *all* the arguments, instead of all but the error,
    resulting in nullary function that never got the error. Fix this
    by adding an input argument substitution.

--- src/rpcserver.cpp
+++ src/rpcserver.cpp
@@ -473,7 +473,7 @@ static void RPCListen(boost::shared_ptr< basic_socket_acceptor<Protocol, SocketA
                 boost::ref(context),
                 fUseSSL,
                 conn,
-                boost::asio::placeholders::error));
+                _1));
 }
 
 
@@ -497,6 +497,8 @@ static void RPCAcceptHandler(boost::shared_ptr< basic_socket_acceptor<Protocol, 
     if (error)
     {
         delete conn;
+        // TODO: Actually handle errors
+        LogPrintf("%s: Error: %s\n", __func__, error.message());
     }
 
     // Restrict callers by IP.  It is important to
