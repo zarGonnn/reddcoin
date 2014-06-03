12ca9fd3206edbd36c6ea1cc3d057b224dabbc53
Wladimir J. van der Laan <laanwj@gmail.com>
2014-06-03 16:12:19 +0200

    Add `-version` option to get just the version
    Adds a `-version` or `--version` option to print just the version
    of the program for bitcoind, bitcoin-cli and bitcoin-qt.
    
    Also make it that `-help` can be used to display the help (as well as
    existing `--help`). Up to now, `-help` was the only option that didn't
    work with either one or two dashes.
    
    Conflicts:
    	src/bitcoin-cli.cpp
    	src/bitcoind.cpp
    	src/qt/utilitydialog.cpp

--- src/bitcoind.cpp
+++ src/bitcoind.cpp
@@ -83,19 +83,22 @@ bool AppInit(int argc, char* argv[])
             return false;
         }
 
-        if (mapArgs.count("-?") || mapArgs.count("--help"))
+        if (mapArgs.count("-?") || mapArgs.count("-help") || mapArgs.count("-version"))
         {
-            // First part of help message is specific to bitcoind / RPC client
-            std::string strUsage = _("Reddcoin version") + " " + FormatFullVersion() + "\n\n" +
-                _("Usage:") + "\n" +
-                  "  reddcoind [options]                     " + _("Start Reddcoin Core Daemon") + "\n" +
-                _("Usage (deprecated, use reddcoin-cli):") + "\n" +
-                  "  reddcoind [options] <command> [params]  " + _("Send command to Reddcoin Core") + "\n" +
-                  "  reddcoind [options] help                " + _("List commands") + "\n" +
-                  "  reddcoind [options] help <command>      " + _("Get help for a command") + "\n";
+            std::string strUsage = _("Reddcoin Core Daemon") + " " + _("version") + " " + FormatFullVersion() + "\n";
 
-            strUsage += "\n" + HelpMessage(HMM_BITCOIND);
-            strUsage += "\n" + HelpMessageCli(false);
+            if (!mapArgs.count("-version"))
+            {
+                strUsage += "\n" + _("Usage:") + "\n" +
+                      "  reddcoind [options]                     " + _("Start Reddcoin Core Daemon") + "\n" +
+                    _("Usage (deprecated, use reddcoin-cli):") + "\n" +
+                      "  reddcoind [options] <command> [params]  " + _("Send command to Reddcoin Core") + "\n" +
+                      "  reddcoind [options] help                " + _("List commands") + "\n" +
+                      "  reddcoind [options] help <command>      " + _("Get help for a command") + "\n";
+
+                strUsage += "\n" + HelpMessage(HMM_BITCOIND);
+                strUsage += "\n" + HelpMessageCli(false);
+            }
 
             fprintf(stdout, "%s", strUsage.c_str());
             return false;
