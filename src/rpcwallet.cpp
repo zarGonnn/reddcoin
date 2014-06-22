40fce78d764f8e03958035497931669d719677e9
JaSK <temp@temp.temp>
2014-06-22 23:29:33 +0200

    fixed bug in ListReceived()

--- src/rpcwallet.cpp
+++ src/rpcwallet.cpp
@@ -998,7 +998,7 @@ Value ListReceived(const Array& params, bool fByAccounts)
                 continue;
 
             isminefilter mine = IsMine(*pwalletMain, address);
-            if(!mine & filter)
+            if(!(mine & filter))
                 continue;
 
             tallyitem& item = mapTally[address];
