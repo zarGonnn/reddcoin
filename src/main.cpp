35ee2ec1b5012db3e29f54a593f3e23b75ae3cba
R E Broadley <rebroad+github@gmail.com>
2014-06-27 11:03:24 +0700

    Display unknown commands received.

--- src/main.cpp
+++ src/main.cpp
@@ -4544,6 +4544,7 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv)
     else
     {
         // Ignore unknown commands for extensibility
+        LogPrint("net", "Unknown command \"%s\" from peer=%d\n", SanitizeString(strCommand), pfrom->id);
     }
 
 
