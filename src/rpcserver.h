f5086100bc1c6780c8f7a6c777e7ddaa1e59db87
jtimon <jtimon@monetize.io>
2014-06-27 13:28:08 +0200

    Replace HexBits with strprintf
    Conflicts:
    	src/rpcserver.h

--- src/rpcserver.h
+++ src/rpcserver.h
@@ -120,7 +120,6 @@ extern double GetDifficulty(const CBlockIndex* blockindex = NULL);
 // PoSV
 extern double GetPoSVKernelPS();
 
-extern std::string HexBits(unsigned int nBits);
 extern std::string HelpRequiringPassphrase();
 extern std::string HelpExampleCli(std::string methodname, std::string args);
 extern std::string HelpExampleRpc(std::string methodname, std::string args);
