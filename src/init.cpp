21e7cbc2c03fec9015e1b2b6a0f5c834dda83ac1
Wladimir J. van der Laan <laanwj@gmail.com>
2014-07-17 14:09:55 +0200

    Revert "Relay double-spends, subject to anti-DOS"
    This reverts commit d640a3ceab4f4372c2a0f738c1286cfde4b41b50.
    
    Conflicts:
    	src/core.h
    	src/main.cpp

--- src/init.cpp
+++ src/init.cpp
@@ -1286,7 +1286,6 @@ bool AppInit2(boost::thread_group& threadGroup)
     LogPrintf("mapAddressBook.size() = %u\n",  pwalletMain ? pwalletMain->mapAddressBook.size() : 0);
 #endif
 
-    RegisterInternalSignals();
     StartNode(threadGroup);
     if (fServer)
         StartRPCThreads();
