9b555450e95c0dc3748b2b40dd98579772269185
paveljanik <Pavel@Janik.cz>
2014-07-23 22:47:18 +0200

    Fix typos in comments (missing i's).

--- src/version.h
+++ src/version.h
@@ -38,7 +38,7 @@ static const int POW_BLOCK_VERSION = 2;
 //
 static const int PROTOCOL_VERSION = 80000;
 
-// intial proto version, to be increased after version/verack negotiation
+// initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
 // disconnect from peers older than this proto version
@@ -55,7 +55,7 @@ static const int NOBLKS_VERSION_END = 32400;
 // BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 60000;
 
-// "mempool" command, enhanced "getdata" behavior starts with this version:
+// "mempool" command, enhanced "getdata" behavior starts with this version
 static const int MEMPOOL_GD_VERSION = 60002;
 
 #endif
