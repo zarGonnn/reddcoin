fba678208dec13586f93d55666a2af9a63b53461
jtimon <jtimon@monetize.io>
2014-08-01 17:45:09 +0200

    Declare SignatureHash() in script.h

--- src/script.h
+++ src/script.h
@@ -810,6 +810,7 @@ bool IsCanonicalSignature(const std::vector<unsigned char> &vchSig, unsigned int
 
 uint256 SignatureHash(const CScript& scriptCode, const CTransaction& txTo, unsigned int nIn, int nHashType);
 bool EvalScript(std::vector<std::vector<unsigned char> >& stack, const CScript& script, const CTransaction& txTo, unsigned int nIn, unsigned int flags, int nHashType);
+uint256 SignatureHash(const CScript &scriptCode, const CTransaction& txTo, unsigned int nIn, int nHashType);
 bool Solver(const CScript& scriptPubKey, txnouttype& typeRet, std::vector<std::vector<unsigned char> >& vSolutionsRet);
 int ScriptSigArgsExpected(txnouttype t, const std::vector<std::vector<unsigned char> >& vSolutions);
 bool IsStandard(const CScript& scriptPubKey, txnouttype& whichType);
