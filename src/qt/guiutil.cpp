hebasto/bitcoin-qtw

qt: make URI scheme comparison case insensitive
Fixes ancient issue #1460.

src/qt/guiutil.cpp
@@ -150,7 +150,7 @@ bool parseBitcoinURI(QString uri, SendCoinsRecipient *out)
     //
     //    Cannot handle this later, because bitcoin:// will cause Qt to see the part after // as host,
     //    which will lower-case it (and thus invalidate the address).
-    if(uri.startsWith("bitcoin://"))
+    if(uri.startsWith("bitcoin://", Qt::CaseInsensitive))
     {
         uri.replace(0, 10, "bitcoin:");
     }

zarGonnn/reddcoin

Conflicts:
	src/qt/guiutil.cpp

src/qt/guiutil.cpp
@@ -150,7 +150,7 @@ bool parseBitcoinURI(QString uri, SendCoinsRecipient *out)
     //
     //    Cannot handle this later, because bitcoin:// will cause Qt to see the part after // as host,
     //    which will lower-case it (and thus invalidate the address).
-    if(uri.startsWith("reddcoin://"))
+    if(uri.startsWith("reddcoin://", Qt::CaseInsensitive))
     {
         uri.replace(0, 11, "reddcoin:");
     }