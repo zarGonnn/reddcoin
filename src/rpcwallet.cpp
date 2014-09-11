Mr-Bossman/VerusCoin

small changes to rpc command help message strings

rpcwallet.cpp
@@ -1850,9 +1850,9 @@ Value settxfee(const Array& params, bool fHelp)
     if (fHelp || params.size() < 1 || params.size() > 1)
         throw runtime_error(
             "settxfee amount\n"
-            "\nSet the transaction fee per KB.\n"
+            "\nSet the transaction fee per kB.\n"
             "\nArguments:\n"
-            "1. amount         (numeric, required) The transaction fee in BTC/KB rounded to the nearest 0.00000001\n"
+            "1. amount         (numeric, required) The transaction fee in BTC/kB rounded to the nearest 0.00000001\n"
             "\nResult\n"
             "true|false        (boolean) Returns true if successful\n"
             "\nExamples:\n"

zarGonnn/reddcoin

Conflicts:
	src/rpcwallet.cpp

src/rpcwallet.cpp
@@ -1962,9 +1962,9 @@ Value settxfee(const Array& params, bool fHelp)
     if (fHelp || params.size() < 1 || params.size() > 1)
         throw runtime_error(
             "settxfee amount\n"
-            "\nSet the transaction fee per KB.\n"
+            "\nSet the transaction fee per kB.\n"
             "\nArguments:\n"
-            "1. amount         (numeric, required) The transaction fee in RDD/KB rounded to the nearest 0.00000001\n"
+            "1. amount         (numeric, required) The transaction fee in RDD/kB rounded to the nearest 0.00000001\n"
             "\nResult\n"
             "true|false        (boolean) Returns true if successful\n"
             "\nExamples:\n"