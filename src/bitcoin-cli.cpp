tmagik/catoshi

Use the new name Bitcoin Core Daemon instead of Bitcoin server
Rebased-By: Wladimir J. van der Laan <laanwj@gmail.com>
Rebased-From-Github-Pull: #3801

src/bitcoin-cli.cpp
@@ -37,9 +37,9 @@ static bool AppInitRPC(int argc, char* argv[])
     if (argc<2 || mapArgs.count("-?") || mapArgs.count("--help"))
     {
         // First part of help message is specific to RPC client
-        std::string strUsage = _("Bitcoin RPC client version") + " " + FormatFullVersion() + "\n\n" +
+        std::string strUsage = _("Bitcoin Core RPC client version") + " " + FormatFullVersion() + "\n\n" +
             _("Usage:") + "\n" +
-              "  bitcoin-cli [options] <command> [params]  " + _("Send command to Bitcoin server") + "\n" +
+              "  bitcoin-cli [options] <command> [params]  " + _("Send command to Bitcoin Core") + "\n" +
               "  bitcoin-cli [options] help                " + _("List commands") + "\n" +
               "  bitcoin-cli [options] help <command>      " + _("Get help for a command") + "\n";
 

src/bitcoind.cpp
@@ -82,9 +82,9 @@ bool AppInit(int argc, char* argv[])
             // First part of help message is specific to bitcoind / RPC client
             std::string strUsage = _("Bitcoin Core Daemon") + " " + _("version") + " " + FormatFullVersion() + "\n\n" +
                 _("Usage:") + "\n" +
-                  "  bitcoind [options]                     " + _("Start Bitcoin server") + "\n" +
+                  "  bitcoind [options]                     " + _("Start Bitcoin Core Daemon") + "\n" +
                 _("Usage (deprecated, use bitcoin-cli):") + "\n" +
-                  "  bitcoind [options] <command> [params]  " + _("Send command to Bitcoin server") + "\n" +
+                  "  bitcoind [options] <command> [params]  " + _("Send command to Bitcoin Core") + "\n" +
                   "  bitcoind [options] help                " + _("List commands") + "\n" +
                   "  bitcoind [options] help <command>      " + _("Get help for a command") + "\n";
 

zarGonnn/reddcoin

Conflicts:
	src/bitcoin-cli.cpp
	src/bitcoind.cpp

src/bitcoin-cli.cpp
@@ -37,9 +37,9 @@ static bool AppInitRPC(int argc, char* argv[])
     if (argc<2 || mapArgs.count("-?") || mapArgs.count("--help"))
     {
         // First part of help message is specific to RPC client
-        std::string strUsage = _("Reddcoin RPC client version") + " " + FormatFullVersion() + "\n\n" +
+        std::string strUsage = _("Reddcoin Core RPC client version") + " " + FormatFullVersion() + "\n\n" +
             _("Usage:") + "\n" +
-              "  reddcoin-cli [options] <command> [params]  " + _("Send command to Reddcoin server") + "\n" +
+              "  reddcoin-cli [options] <command> [params]  " + _("Send command to Reddcoin Core") + "\n" +
               "  reddcoin-cli [options] help                " + _("List commands") + "\n" +
               "  reddcoin-cli [options] help <command>      " + _("Get help for a command") + "\n";
 

src/bitcoind.cpp
@@ -82,9 +82,9 @@ bool AppInit(int argc, char* argv[])
             // First part of help message is specific to bitcoind / RPC client
             std::string strUsage = _("Reddcoin version") + " " + FormatFullVersion() + "\n\n" +
                 _("Usage:") + "\n" +
-                  "  reddcoind [options]                     " + _("Start Reddcoin server") + "\n" +
+                  "  reddcoind [options]                     " + _("Start Reddcoin Core Daemon") + "\n" +
                 _("Usage (deprecated, use reddcoin-cli):") + "\n" +
-                  "  reddcoind [options] <command> [params]  " + _("Send command to Reddcoin server") + "\n" +
+                  "  reddcoind [options] <command> [params]  " + _("Send command to Reddcoin Core") + "\n" +
                   "  reddcoind [options] help                " + _("List commands") + "\n" +
                   "  reddcoind [options] help <command>      " + _("Get help for a command") + "\n";
 