ca51c38a96311e102acec7af30435d72f067b04b
John Nash <redd@redd.ink>
2016-01-25 12:55:57 +1100

    Reddcoin:Base Fix some QT labels
    Correctly display balance on SendCoins Dialogue

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -71,9 +71,9 @@ qint64 BitcoinUnits::maxAmount(int unit)
 {
     switch(unit)
     {
-    case BTC:  return Q_INT64_C(21000000);
-    case mBTC: return Q_INT64_C(21000000000);
-    case uBTC: return Q_INT64_C(21000000000000);
+    case BTC:  return Q_INT64_C(99999999);
+    case mBTC: return Q_INT64_C(99999999999);
+    case uBTC: return Q_INT64_C(99999999999999);
     default:   return 0;
     }
 }
@@ -113,6 +113,14 @@ QString BitcoinUnits::format(int unit, qint64 n, bool fPlus)
     qint64 remainder = n_abs % coin;
     QString quotient_str = QString::number(quotient);
     QString remainder_str = QString::number(remainder).rightJustified(num_decimals, '0');
+
+    // Adds spaces every three characters of quotient_str
+    // Example: 123456 is now 123 456
+    for(int i = quotient_str.count(); i > 3;i-=3)
+    {
+        if (i > 3)
+            quotient_str.insert(i-3,' ');
+    }
 
     // Right-trim excess zeros after the decimal point
     int nTrim = 0;
