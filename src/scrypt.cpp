c2dd0a4f6e4db2cb640aa6b322765814c55796a7
John Nash <redd@redd.ink>
2016-05-13 13:06:36 +1000

    Reddcoin:Base updated printf to LogPrintf to redirect debug messages to logfile instead of console

--- src/scrypt.cpp
+++ src/scrypt.cpp
@@ -395,7 +395,7 @@ void (*scrypt_1024_1_1_256_sp_detected)(const char *input, char *output, char *s
 void scrypt_detect_sse2()
 {
 #if defined(USE_SSE2_ALWAYS)
-    printf("scrypt: using scrypt-sse2 as built.\n");
+    LogPrintf("scrypt: using scrypt-sse2 as built.\n");
 #else // USE_SSE2_ALWAYS
     // 32bit x86 Linux or Windows, detect cpuid features
     unsigned int cpuid_edx=0;
@@ -413,12 +413,12 @@ void scrypt_detect_sse2()
     if (cpuid_edx & 1<<26)
     {
         scrypt_1024_1_1_256_sp_detected = &scrypt_1024_1_1_256_sp_sse2;
-        printf("scrypt: using scrypt-sse2 as detected.\n");
+        LogPrintf("scrypt: using scrypt-sse2 as detected.\n");
     }
     else
     {
         scrypt_1024_1_1_256_sp_detected = &scrypt_1024_1_1_256_sp_generic;
-        printf("scrypt: using scrypt-generic, SSE2 unavailable.\n");
+        LogPrintf("scrypt: using scrypt-generic, SSE2 unavailable.\n");
     }
 #endif // USE_SSE2_ALWAYS
 }
