77e8557be406bc8311747830c78474b148a15c7e
John Nash <redd@redd.ink>
2016-06-15 23:05:46 +1000

    Remove unused function WildcardMatch
    No longer necessary after implementing netmask-based matching.
    Also remove a longer-unused function `skipspaces`.
    
    (cherry picked from bitcoin commit fdbd7075cab8d54f90c038e68afba868a9ff9f63)

--- src/util.h
+++ src/util.h
@@ -170,8 +170,6 @@ std::string DecodeBase32(const std::string& str);
 std::string EncodeBase32(const unsigned char* pch, size_t len);
 std::string EncodeBase32(const std::string& str);
 void ParseParameters(int argc, const char*const argv[]);
-bool WildcardMatch(const char* psz, const char* mask);
-bool WildcardMatch(const std::string& str, const std::string& mask);
 void FileCommit(FILE *fileout);
 bool TruncateFile(FILE *file, unsigned int length);
 int RaiseFileDescriptorLimit(int nMinFD);
@@ -332,13 +330,6 @@ std::string DateTimeStrFormat(const char* pszFormat, int64_t nTime);
 inline std::string DateTimeStrFormat(int64_t nTime)
 {
     return DateTimeStrFormat("%Y-%m-%d %H:%M:%S UTC", nTime);
-}
-
-template<typename T>
-void skipspaces(T& it)
-{
-    while (isspace(*it))
-        ++it;
 }
 
 inline bool IsSwitchChar(char c)
