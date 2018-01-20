8f41ec0ac927c9c915cb52c805dccf548fdb59a0
John Nash <redd@redd.ink>
2018-01-20 19:55:41 +1100

    build: adding LIBTOOLIZE

--- autogen.sh
+++ autogen.sh
@@ -2,4 +2,10 @@
 set -e
 srcdir="$(dirname $0)"
 cd "$srcdir"
+
+if [ -z ${LIBTOOLIZE} ] && GLIBTOOLIZE="`which glibtoolize 2>/dev/null`"; then
+  LIBTOOLIZE="${GLIBTOOLIZE}"
+  export LIBTOOLIZE
+fi
+
 autoreconf --install --force
