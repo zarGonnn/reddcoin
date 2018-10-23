f0cb5eed81d40e8d4d7f79d68dc907da421d3466
Yavuz Selim <yavuzselim@gmail.com>
2018-10-23 03:07:34 +0200

    Update scrypt.cpp
    Commented out SSE2 functions, to compile successfully on Raspberry Pi 3B.
    
    Source: https://github.com/reddcoin-project/reddcoin/compare/master...joroob:master#diff-0ad12b987110c57eab056e4e5e6e9cc5

--- src/scrypt.cpp
+++ src/scrypt.cpp
@@ -34,17 +34,17 @@
 #include <string.h>
 #include <openssl/sha.h>
 
-#include <emmintrin.h>
-
-#if defined(USE_SSE2) && !defined(USE_SSE2_ALWAYS)
-#ifdef _MSC_VER
-// MSVC 64bit is unable to use inline asm
-#include <intrin.h>
-#else
-// GCC Linux or i686-w64-mingw32
-#include <cpuid.h>
-#endif
-#endif
+//#include <emmintrin.h>
+//
+//#if defined(USE_SSE2) && !defined(USE_SSE2_ALWAYS)
+//#ifdef _MSC_VER
+//// MSVC 64bit is unable to use inline asm
+//#include <intrin.h>
+//#else
+//// GCC Linux or i686-w64-mingw32
+//#include <cpuid.h>
+//#endif
+//#endif
 
 static inline uint32_t be32dec(const void *pp)
 {
@@ -288,141 +288,141 @@ void scrypt_1024_1_1_256_sp_generic(const char *input, char *output, char *scrat
 	PBKDF2_SHA256((const uint8_t *)input, 80, B, 128, 1, (uint8_t *)output, 32);
 }
 
-static inline void xor_salsa8_sse2(__m128i B[4], const __m128i Bx[4])
-{
-	__m128i X0, X1, X2, X3;
-	__m128i T;
-	int i;
-
-	X0 = B[0] = _mm_xor_si128(B[0], Bx[0]);
-	X1 = B[1] = _mm_xor_si128(B[1], Bx[1]);
-	X2 = B[2] = _mm_xor_si128(B[2], Bx[2]);
-	X3 = B[3] = _mm_xor_si128(B[3], Bx[3]);
-
-	for (i = 0; i < 8; i += 2) {
-		/* Operate on "columns". */
-		T = _mm_add_epi32(X0, X3);
-		X1 = _mm_xor_si128(X1, _mm_slli_epi32(T, 7));
-		X1 = _mm_xor_si128(X1, _mm_srli_epi32(T, 25));
-		T = _mm_add_epi32(X1, X0);
-		X2 = _mm_xor_si128(X2, _mm_slli_epi32(T, 9));
-		X2 = _mm_xor_si128(X2, _mm_srli_epi32(T, 23));
-		T = _mm_add_epi32(X2, X1);
-		X3 = _mm_xor_si128(X3, _mm_slli_epi32(T, 13));
-		X3 = _mm_xor_si128(X3, _mm_srli_epi32(T, 19));
-		T = _mm_add_epi32(X3, X2);
-		X0 = _mm_xor_si128(X0, _mm_slli_epi32(T, 18));
-		X0 = _mm_xor_si128(X0, _mm_srli_epi32(T, 14));
-
-		/* Rearrange data. */
-		X1 = _mm_shuffle_epi32(X1, 0x93);
-		X2 = _mm_shuffle_epi32(X2, 0x4E);
-		X3 = _mm_shuffle_epi32(X3, 0x39);
-
-		/* Operate on "rows". */
-		T = _mm_add_epi32(X0, X1);
-		X3 = _mm_xor_si128(X3, _mm_slli_epi32(T, 7));
-		X3 = _mm_xor_si128(X3, _mm_srli_epi32(T, 25));
-		T = _mm_add_epi32(X3, X0);
-		X2 = _mm_xor_si128(X2, _mm_slli_epi32(T, 9));
-		X2 = _mm_xor_si128(X2, _mm_srli_epi32(T, 23));
-		T = _mm_add_epi32(X2, X3);
-		X1 = _mm_xor_si128(X1, _mm_slli_epi32(T, 13));
-		X1 = _mm_xor_si128(X1, _mm_srli_epi32(T, 19));
-		T = _mm_add_epi32(X1, X2);
-		X0 = _mm_xor_si128(X0, _mm_slli_epi32(T, 18));
-		X0 = _mm_xor_si128(X0, _mm_srli_epi32(T, 14));
-
-		/* Rearrange data. */
-		X1 = _mm_shuffle_epi32(X1, 0x39);
-		X2 = _mm_shuffle_epi32(X2, 0x4E);
-		X3 = _mm_shuffle_epi32(X3, 0x93);
-	}
-
-	B[0] = _mm_add_epi32(B[0], X0);
-	B[1] = _mm_add_epi32(B[1], X1);
-	B[2] = _mm_add_epi32(B[2], X2);
-	B[3] = _mm_add_epi32(B[3], X3);
-}
-
-void scrypt_1024_1_1_256_sp_sse2(const char *input, char *output, char *scratchpad)
-{
-	uint8_t B[128];
-	union {
-		__m128i i128[8];
-		uint32_t u32[32];
-	} X;
-	__m128i *V;
-	uint32_t i, j, k;
-
-	V = (__m128i *)(((uintptr_t)(scratchpad) + 63) & ~ (uintptr_t)(63));
-
-	PBKDF2_SHA256((const uint8_t *)input, 80, (const uint8_t *)input, 80, 1, B, 128);
-
-	for (k = 0; k < 2; k++) {
-		for (i = 0; i < 16; i++) {
-			X.u32[k * 16 + i] = le32dec(&B[(k * 16 + (i * 5 % 16)) * 4]);
-		}
-	}
-
-	for (i = 0; i < 1024; i++) {
-		for (k = 0; k < 8; k++)
-			V[i * 8 + k] = X.i128[k];
-		xor_salsa8_sse2(&X.i128[0], &X.i128[4]);
-		xor_salsa8_sse2(&X.i128[4], &X.i128[0]);
-	}
-	for (i = 0; i < 1024; i++) {
-		j = 8 * (X.u32[16] & 1023);
-		for (k = 0; k < 8; k++)
-			X.i128[k] = _mm_xor_si128(X.i128[k], V[j + k]);
-		xor_salsa8_sse2(&X.i128[0], &X.i128[4]);
-		xor_salsa8_sse2(&X.i128[4], &X.i128[0]);
-	}
-
-	for (k = 0; k < 2; k++) {
-		for (i = 0; i < 16; i++) {
-			le32enc(&B[(k * 16 + (i * 5 % 16)) * 4], X.u32[k * 16 + i]);
-		}
-	}
-
-	PBKDF2_SHA256((const uint8_t *)input, 80, B, 128, 1, (uint8_t *)output, 32);
-}
-
-#if defined(USE_SSE2)
-// By default, set to generic scrypt function. This will prevent crash in case when scrypt_detect_sse2() wasn't called
-void (*scrypt_1024_1_1_256_sp_detected)(const char *input, char *output, char *scratchpad) = &scrypt_1024_1_1_256_sp_generic;
-
-void scrypt_detect_sse2()
-{
-#if defined(USE_SSE2_ALWAYS)
-    LogPrintf("scrypt: using scrypt-sse2 as built.\n");
-#else // USE_SSE2_ALWAYS
-    // 32bit x86 Linux or Windows, detect cpuid features
-    unsigned int cpuid_edx=0;
-#if defined(_MSC_VER)
-    // MSVC
-    int x86cpuid[4];
-    __cpuid(x86cpuid, 1);
-    cpuid_edx = (unsigned int)buffer[3];
-#else // _MSC_VER
-    // Linux or i686-w64-mingw32 (gcc-4.6.3)
-    unsigned int eax, ebx, ecx;
-    __get_cpuid(1, &eax, &ebx, &ecx, &cpuid_edx);
-#endif // _MSC_VER
-
-    if (cpuid_edx & 1<<26)
-    {
-        scrypt_1024_1_1_256_sp_detected = &scrypt_1024_1_1_256_sp_sse2;
-        LogPrintf("scrypt: using scrypt-sse2 as detected.\n");
-    }
-    else
-    {
-        scrypt_1024_1_1_256_sp_detected = &scrypt_1024_1_1_256_sp_generic;
-        LogPrintf("scrypt: using scrypt-generic, SSE2 unavailable.\n");
-    }
-#endif // USE_SSE2_ALWAYS
-}
-#endif
+//static inline void xor_salsa8_sse2(__m128i B[4], const __m128i Bx[4])
+//{
+//	__m128i X0, X1, X2, X3;
+//	__m128i T;
+//	int i;
+//
+//	X0 = B[0] = _mm_xor_si128(B[0], Bx[0]);
+//	X1 = B[1] = _mm_xor_si128(B[1], Bx[1]);
+//	X2 = B[2] = _mm_xor_si128(B[2], Bx[2]);
+//	X3 = B[3] = _mm_xor_si128(B[3], Bx[3]);
+//
+//	for (i = 0; i < 8; i += 2) {
+//		/* Operate on "columns". */
+//		T = _mm_add_epi32(X0, X3);
+//		X1 = _mm_xor_si128(X1, _mm_slli_epi32(T, 7));
+//		X1 = _mm_xor_si128(X1, _mm_srli_epi32(T, 25));
+//		T = _mm_add_epi32(X1, X0);
+//		X2 = _mm_xor_si128(X2, _mm_slli_epi32(T, 9));
+//		X2 = _mm_xor_si128(X2, _mm_srli_epi32(T, 23));
+//		T = _mm_add_epi32(X2, X1);
+//		X3 = _mm_xor_si128(X3, _mm_slli_epi32(T, 13));
+//		X3 = _mm_xor_si128(X3, _mm_srli_epi32(T, 19));
+//		T = _mm_add_epi32(X3, X2);
+//		X0 = _mm_xor_si128(X0, _mm_slli_epi32(T, 18));
+//		X0 = _mm_xor_si128(X0, _mm_srli_epi32(T, 14));
+//
+//		/* Rearrange data. */
+//		X1 = _mm_shuffle_epi32(X1, 0x93);
+//		X2 = _mm_shuffle_epi32(X2, 0x4E);
+//		X3 = _mm_shuffle_epi32(X3, 0x39);
+//
+//		/* Operate on "rows". */
+//		T = _mm_add_epi32(X0, X1);
+//		X3 = _mm_xor_si128(X3, _mm_slli_epi32(T, 7));
+//		X3 = _mm_xor_si128(X3, _mm_srli_epi32(T, 25));
+//		T = _mm_add_epi32(X3, X0);
+//		X2 = _mm_xor_si128(X2, _mm_slli_epi32(T, 9));
+//		X2 = _mm_xor_si128(X2, _mm_srli_epi32(T, 23));
+//		T = _mm_add_epi32(X2, X3);
+//		X1 = _mm_xor_si128(X1, _mm_slli_epi32(T, 13));
+//		X1 = _mm_xor_si128(X1, _mm_srli_epi32(T, 19));
+//		T = _mm_add_epi32(X1, X2);
+//		X0 = _mm_xor_si128(X0, _mm_slli_epi32(T, 18));
+//		X0 = _mm_xor_si128(X0, _mm_srli_epi32(T, 14));
+//
+//		/* Rearrange data. */
+//		X1 = _mm_shuffle_epi32(X1, 0x39);
+//		X2 = _mm_shuffle_epi32(X2, 0x4E);
+//		X3 = _mm_shuffle_epi32(X3, 0x93);
+//	}
+//
+//	B[0] = _mm_add_epi32(B[0], X0);
+//	B[1] = _mm_add_epi32(B[1], X1);
+//	B[2] = _mm_add_epi32(B[2], X2);
+//	B[3] = _mm_add_epi32(B[3], X3);
+//}
+//
+//void scrypt_1024_1_1_256_sp_sse2(const char *input, char *output, char *scratchpad)
+//{
+//	uint8_t B[128];
+//	union {
+//		__m128i i128[8];
+//		uint32_t u32[32];
+//	} X;
+//	__m128i *V;
+//	uint32_t i, j, k;
+//
+//	V = (__m128i *)(((uintptr_t)(scratchpad) + 63) & ~ (uintptr_t)(63));
+//
+//	PBKDF2_SHA256((const uint8_t *)input, 80, (const uint8_t *)input, 80, 1, B, 128);
+//
+//	for (k = 0; k < 2; k++) {
+//		for (i = 0; i < 16; i++) {
+//			X.u32[k * 16 + i] = le32dec(&B[(k * 16 + (i * 5 % 16)) * 4]);
+//		}
+//	}
+//
+//	for (i = 0; i < 1024; i++) {
+//		for (k = 0; k < 8; k++)
+//			V[i * 8 + k] = X.i128[k];
+//		xor_salsa8_sse2(&X.i128[0], &X.i128[4]);
+//		xor_salsa8_sse2(&X.i128[4], &X.i128[0]);
+//	}
+//	for (i = 0; i < 1024; i++) {
+//		j = 8 * (X.u32[16] & 1023);
+//		for (k = 0; k < 8; k++)
+//			X.i128[k] = _mm_xor_si128(X.i128[k], V[j + k]);
+//		xor_salsa8_sse2(&X.i128[0], &X.i128[4]);
+//		xor_salsa8_sse2(&X.i128[4], &X.i128[0]);
+//	}
+//
+//	for (k = 0; k < 2; k++) {
+//		for (i = 0; i < 16; i++) {
+//			le32enc(&B[(k * 16 + (i * 5 % 16)) * 4], X.u32[k * 16 + i]);
+//		}
+//	}
+//
+//	PBKDF2_SHA256((const uint8_t *)input, 80, B, 128, 1, (uint8_t *)output, 32);
+//}
+//
+//#if defined(USE_SSE2)
+//// By default, set to generic scrypt function. This will prevent crash in case when scrypt_detect_sse2() wasn't called
+//void (*scrypt_1024_1_1_256_sp_detected)(const char *input, char *output, char *scratchpad) = &scrypt_1024_1_1_256_sp_generic;
+//
+//void scrypt_detect_sse2()
+//{
+//#if defined(USE_SSE2_ALWAYS)
+//    LogPrintf("scrypt: using scrypt-sse2 as built.\n");
+//#else // USE_SSE2_ALWAYS
+//    // 32bit x86 Linux or Windows, detect cpuid features
+//    unsigned int cpuid_edx=0;
+//#if defined(_MSC_VER)
+//    // MSVC
+//    int x86cpuid[4];
+//    __cpuid(x86cpuid, 1);
+//    cpuid_edx = (unsigned int)buffer[3];
+//#else // _MSC_VER
+//    // Linux or i686-w64-mingw32 (gcc-4.6.3)
+//    unsigned int eax, ebx, ecx;
+//    __get_cpuid(1, &eax, &ebx, &ecx, &cpuid_edx);
+//#endif // _MSC_VER
+//
+//    if (cpuid_edx & 1<<26)
+//    {
+//        scrypt_1024_1_1_256_sp_detected = &scrypt_1024_1_1_256_sp_sse2;
+//        LogPrintf("scrypt: using scrypt-sse2 as detected.\n");
+//    }
+//    else
+//    {
+//        scrypt_1024_1_1_256_sp_detected = &scrypt_1024_1_1_256_sp_generic;
+//        LogPrintf("scrypt: using scrypt-generic, SSE2 unavailable.\n");
+//    }
+//#endif // USE_SSE2_ALWAYS
+//}
+//#endif
 
 void scrypt_1024_1_1_256(const char *input, char *output)
 {
